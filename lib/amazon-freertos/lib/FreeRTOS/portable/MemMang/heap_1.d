73f1d8556535899970d00b317906eaa68b8e6551
Bery Saidi <abdelberysaidi@gmail.com>
2019-04-08 22:48:35 -0400

    added submodules

---
+++ lib/amazon-freertos/lib/FreeRTOS/portable/MemMang/heap_1.d
@@ -0,0 +1,29 @@
+lib/amazon-freertos/lib/FreeRTOS/portable/MemMang/heap_1.o: \
+ lib/amazon-freertos/lib/FreeRTOS/portable/MemMang/heap_1.c \
+ /usr/arm-none-eabi/include/stdlib.h \
+ /usr/arm-none-eabi/include/machine/ieeefp.h \
+ /usr/arm-none-eabi/include/_ansi.h /usr/arm-none-eabi/include/newlib.h \
+ /usr/arm-none-eabi/include/_newlib_version.h \
+ /usr/arm-none-eabi/include/sys/config.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/8.3.0/include/stddef.h \
+ /usr/arm-none-eabi/include/sys/reent.h \
+ /usr/arm-none-eabi/include/_ansi.h \
+ /usr/arm-none-eabi/include/sys/_types.h \
+ /usr/arm-none-eabi/include/machine/_types.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/lock.h \
+ /usr/arm-none-eabi/include/sys/cdefs.h \
+ /usr/arm-none-eabi/include/machine/stdlib.h \
+ lib/amazon-freertos/lib/include/FreeRTOS.h \
+ /usr/lib/gcc/arm-none-eabi/8.3.0/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /usr/arm-none-eabi/include/sys/_stdint.h inc/FreeRTOSConfig.h \
+ lib/amazon-freertos/lib/include/private/projdefs.h \
+ lib/amazon-freertos/lib/include/private/portable.h \
+ lib/amazon-freertos/lib/include/private/deprecated_definitions.h \
+ lib/amazon-freertos/lib/FreeRTOS/portable/GCC/ARM_CM3/portmacro.h \
+ lib/amazon-freertos/lib/include/private/mpu_wrappers.h \
+ lib/amazon-freertos/lib/include/task.h \
+ lib/amazon-freertos/lib/include/private/list.h
