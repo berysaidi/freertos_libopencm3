fa07db6b07a278ab8ac9b8f7e18cc4de9b0df61c
Bery Saidi <abdelberysaidi@gmail.com>
2019-04-08 22:46:58 -0400

    button task.

--- main.c
+++ main.c
@@ -1,27 +1,60 @@
 /*
- * This file is part of the libopencm3 project.
- *
- * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
- * Copyright (C) 2011 Stephen Caudle <scaudle@doceme.com>
- *
- * This library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <stdint.h>
 #include "FreeRTOS.h"
 #include "task.h"
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+/* Dimensions the buffer that the task being created will use as its stack.
+NOTE:  This is the number of words the stack will hold, not the number of
+bytes.  For example, if each stack item is 32-bits, and this is set to 100,
+then 400 bytes (100 * 32-bits) will be allocated. */
+#define STACK_SIZE 200
+static void
+task1(void *args __attribute((unused))) {
+
+	for (;;) {
+		gpio_toggle(GPIOD,GPIO12);
+		vTaskDelay(pdMS_TO_TICKS(500));
+	}
+}
+/* task to read the user button and check it out */
+static void 
+task_user_button(void *args __attribute((unused))) {
+	while(1){
+		if(0 != gpio_get(GPIOA, GPIO0)){	
+			gpio_toggle(GPIOD,GPIO12);
+		}
+
+	}
+}
+
+static void hardware_setup(void)
+{
+	/* Enable RCC with external crystal */
+	rcc_clock_setup_hse_3v3(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
+	/* Enable GPIOD clock. (LED) */
+	rcc_periph_clock_enable(RCC_GPIOD);
+	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
+	/* Enable GPIOA clock (Button) */
+	rcc_periph_clock_enable(RCC_GPIOA);
+	gpio_mode_setup(GPIOA, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO0);
+
+}
+int main(void)
+{
+	hardware_setup();
+
+	//xTaskCreate(task1,"LED",STACK_SIZE, NULL, configMAX_PRIORITIES-1,NULL);
+	xTaskCreate(task_user_button,"button",STACK_SIZE, NULL, configMAX_PRIORITIES-1,NULL);
+	vTaskStartScheduler();
+	/* Blink the LED (PC8) on the board. */
+	while (1) {
+
+	}
+
+	return 0;
+}
 
 extern void vApplicationStackOverflowHook(
 	TaskHandle_t *pxTask,
@@ -35,58 +68,4 @@ vApplicationStackOverflowHook(
 	for(;;);	// Loop forever here..
 }
 
-static void
-task1(void *args __attribute((unused))) {
-
-	for (;;) {
-		gpio_toggle(GPIOD,GPIO12);
-		vTaskDelay(pdMS_TO_TICKS(500));
-	}
-}
-
-
-
-static void gpio_setup(void)
-{
-	/* Enable GPIOD clock. */
-	/* Manually: */
-	// RCC_AHB1ENR |= RCC_AHB1ENR_IOPDEN;
-	/* Using API functions: */
-	rcc_periph_clock_enable(RCC_GPIOD);
-
-	/* Set GPIO12 (in GPIO port D) to 'output push-pull'. */
-	/* Manually: */
-	// GPIOD_CRH = (GPIO_CNF_OUTPUT_PUSHPULL << (((8 - 8) * 4) + 2));
-	// GPIOD_CRH |= (GPIO_MODE_OUTPUT_2_MHZ << ((8 - 8) * 4));
-	/* Using API functions: */
-	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
-
-}
-    /* Dimensions the buffer that the task being created will use as its stack.
-    NOTE:  This is the number of words the stack will hold, not the number of
-    bytes.  For example, if each stack item is 32-bits, and this is set to 100,
-    then 400 bytes (100 * 32-bits) will be allocated. */
-    #define STACK_SIZE 200
-
-    /* Structure that will hold the TCB of the task being created. */
-    StaticTask_t xTaskBuffer;
-
-    /* Buffer that the task being created will use as its stack.  Note this is
-    an array of StackType_t variables.  The size of StackType_t is dependent on
-    the RTOS port. */
-    StackType_t xStack[ STACK_SIZE ];
-int main(void)
-{
-	rcc_clock_setup_hse_3v3(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
-	gpio_setup();
-
-	xTaskCreate(task1,"LED",STACK_SIZE, NULL, configMAX_PRIORITIES-1,NULL);
-	vTaskStartScheduler();
-	/* Blink the LED (PC8) on the board. */
-	while (1) {
-
-	}
-
-	return 0;
-}
 
